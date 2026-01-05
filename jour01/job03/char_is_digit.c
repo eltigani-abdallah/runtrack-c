7d70e18329aafc2a6d3a085f83d6b98d03fc4f7a
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2026-01-05 14:30:14 +0100

    feat: finished char_is_digit

---
+++ jour01/job03/char_is_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int char_is_digit(char c) {
+    if (c >= '0' && c <= '9') {
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    printf("result of 0 is: %d\n", char_is_digit('5'));
+    printf("result of A is: %d\n", char_is_digit('A'));
+    return 0;
+}
