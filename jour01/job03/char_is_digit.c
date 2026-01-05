a364f46c16728a5383ccc9179bca6c80d6e00f34
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2026-01-05 14:31:18 +0100

    chore: comment out main function and includes after manual testing

--- jour01/job03/char_is_digit.c
+++ jour01/job03/char_is_digit.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 int char_is_digit(char c) {
     if (c >= '0' && c <= '9') {
@@ -6,9 +6,10 @@ int char_is_digit(char c) {
     }
     return 0;
 }
-
+/*
 int main() {
     printf("result of 0 is: %d\n", char_is_digit('5'));
     printf("result of A is: %d\n", char_is_digit('A'));
     return 0;
 }
+*/
